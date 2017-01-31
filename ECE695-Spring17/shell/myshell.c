784adbd45e4a0b550bea0e7a6de40393355319df
Yizhou Shan <shan13@purdue.edu>
2017-01-31 00:27:36 -0500

    hmm, only 46/65 works

--- ECE695-Spring17/shell/myshell.c
+++ ECE695-Spring17/shell/myshell.c
@@ -24,6 +24,31 @@
  */
 void report_background_job(int job_id, int process_id);
 
+static bool builtin_cmd(char *name)
+{
+	if (!strcmp(name, "cd") || !strcmp(name, "exit"))
+		return true;
+	return false;
+}
+
+static int do_builtin(command_t *cmd)
+{
+	char *name = cmd->argv[0];
+
+	if (!strcmp(name, "cd")) {
+		int ret;
+
+		ret = chdir(cmd->argv[1]);
+		if (ret < 0)
+			return ret;
+	} else if (!strcmp(name, "exit")) {
+		exit(0);
+	} else
+		die("BUG");
+
+	return 0;
+}
+
 /* command_exec(cmd, pass_pipefd)
  *
  *   Execute the single command specified in the 'cmd' command structure.
@@ -55,7 +80,8 @@ void report_background_job(int job_id, int process_id);
  *   You should divide functionality between the parent and the child.
  *   Some functions will be executed in each process.
  */
-static pid_t command_exec(command_t *cmd, int *pass_pipefd)
+static pid_t command_exec(command_t *cmd, int *pass_pipefd, bool *builtin,
+			  int *builtin_status)
 {
 	pid_t pid = -1;
 	int fd[2];
@@ -106,6 +132,56 @@ static pid_t command_exec(command_t *cmd, int *pass_pipefd)
 	//    in which directory would foo appear, /tmp or $HOME?
 	//
 
+	if (builtin_cmd(cmd->argv[0])) {
+		if (cmd->controlop == CMD_PIPE) {
+			close(fd[0]);
+			if (fd[1] != STDOUT_FILENO) {
+				if (dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO)
+					die("error");
+				close(fd[1]);
+			}
+		} else if (cmd->redirect_filename[STDOUT_FILENO]) {
+			int fd;
+			char *fn;
+
+			fn = cmd->redirect_filename[STDOUT_FILENO];
+			fd = open(fn, O_RDWR | O_CREAT, 0644);
+			if (fd < 0)
+				die("fail to open %s\n", fn);
+
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		if (*pass_pipefd != STDIN_FILENO) {
+			dup2(*pass_pipefd, STDIN_FILENO);
+			close(*pass_pipefd);
+		} else if (cmd->redirect_filename[STDIN_FILENO]) {
+			int fd;
+			char *fn;
+			
+			fn = cmd->redirect_filename[STDIN_FILENO];
+			fd = open(fn, O_RDWR, 0644);
+			if (fd < 0)
+				die("fail to open %s\n", fn);
+
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+
+		if (cmd->controlop == CMD_PIPE) {
+			close(fd[1]);
+			*pass_pipefd = fd[0];
+		} else
+			*pass_pipefd = STDIN_FILENO;
+
+		*builtin = true;
+		*builtin_status = do_builtin(cmd);
+		return 0; /* Useless */
+	}
+
+	/*
+	 * Normal non-builtin command
+	 */
 	if ((pid = fork()) < 0) {
 		die("Fail to fork");
 		return pid;
@@ -120,7 +196,12 @@ static pid_t command_exec(command_t *cmd, int *pass_pipefd)
 		 */
 		if (cmd->controlop == CMD_PIPE) {
 			close(fd[0]);
-			dup2(fd[1], STDOUT_FILENO);
+			if (fd[1] )
+			if (fd[1] != STDOUT_FILENO) {
+				if (dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO)
+					die("error");
+				close(fd[1]);
+			}
 		} else if (cmd->redirect_filename[STDOUT_FILENO]) {
 			int fd;
 			char *fn;
@@ -131,6 +212,7 @@ static pid_t command_exec(command_t *cmd, int *pass_pipefd)
 				die("fail to open %s\n", fn);
 
 			dup2(fd, STDOUT_FILENO);
+			close(fd);
 		}
 
 		/*
@@ -142,6 +224,7 @@ static pid_t command_exec(command_t *cmd, int *pass_pipefd)
 		if (*pass_pipefd != STDIN_FILENO) {
 			/* The right-side of the pipe, CMD | CMD */
 			dup2(*pass_pipefd, STDIN_FILENO);
+			close(*pass_pipefd);
 		} else if (cmd->redirect_filename[STDIN_FILENO]) {
 			int fd;
 			char *fn;
@@ -152,12 +235,12 @@ static pid_t command_exec(command_t *cmd, int *pass_pipefd)
 				die("fail to open %s\n", fn);
 
 			dup2(fd, STDIN_FILENO);
+			close(fd);
 		}
 
 		execvp(cmd->argv[0], cmd->argv);
 	} else {
 		/* Parent */
-
 		if (cmd->controlop == CMD_PIPE) {
 			close(fd[1]);
 			*pass_pipefd = fd[0];
@@ -201,26 +284,107 @@ int command_line_exec(command_t *cmdlist)
 	while (cmdlist) {
 		pid_t pid;
 		int wp_status;
+		bool builtin = false;
+		int builtin_status = 0;
 
+		/*
+		 * Handle Sub Shell
+		 */
 		if (cmdlist->subshell) {
-			cmd_status = command_line_exec(cmdlist->subshell);
+			pid_t pid;
+			int fd[2];
+			int sub_cmd_status, sub_wp_status;
 
-			if ((cmdlist->controlop == CMD_AND && !cmd_status) ||
-			    (cmdlist->controlop == CMD_OR && cmd_status))
-				goto next;
+			/* ( CMD ) | CMD */
+			if (cmdlist->controlop == CMD_PIPE) {
+				if (pipe(fd) < 0)
+					die("Fail to create pipe");
+			}
+
+			if ((pid = fork()) < 0)
+				die("Fail to fork() for subshell");
+			else if (pid == 0) {
+				/* Subshell */
+				if (cmdlist->controlop == CMD_PIPE) {
+					close(fd[0]);
+					if (fd[1] != STDOUT_FILENO) {
+						if (dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO)
+							die("error");
+						close(fd[1]);
+					}
+				}
+
+				sub_cmd_status = command_line_exec(cmdlist->subshell);
+				exit(sub_cmd_status);
+			} else {
+				/* Parent Shell */
+				if (cmdlist->controlop == CMD_PIPE) {
+					close(fd[1]);
+					if (fd[0] != STDIN_FILENO) {
+						if (dup2(fd[0], STDIN_FILENO) != STDIN_FILENO)
+							die("dup2 fails");
+						close(fd[0]);
+					}
+				}
+
+				if (waitpid(pid, &sub_wp_status, 0) < 0)
+					die("Fail to wait subshell");
+
+				if (WIFEXITED(wp_status))
+					sub_cmd_status = WEXITSTATUS(wp_status);
+				else if (WIFSTOPPED(wp_status)) {
+					;
+					//printf("The subshell has not terminated\n");
+				} else if (WIFSIGNALED(wp_status)) {
+					sub_cmd_status = 0;
+					//printf("The subshell terminated due to signal: %d\n",
+					//	WTERMSIG(wp_status));
+				}
+
+				/*
+				 * ( CMD ) && CMD
+				 * ( CMD ) || CMD
+				 * ( CMD ) ;
+				 * ( CMD ) | CMD
+				 */
+				if ((cmdlist->controlop == CMD_AND && !sub_cmd_status)	||
+				    (cmdlist->controlop == CMD_OR && sub_cmd_status)	||
+				    (cmdlist->controlop == CMD_END)			||
+				    (cmdlist->controlop == CMD_SEMICOLON)               ||
+				    (cmdlist->controlop == CMD_PIPE)                    ||
+				    (cmdlist->controlop == CMD_BACKGROUND))
+					goto next;
+				else
+					goto out;
+			}
 		}
 
-		pid = command_exec(cmdlist, &pipefd);
+		/*
+		 * Handle Normal Linked Command
+		 */
+		pid = command_exec(cmdlist, &pipefd, &builtin, &builtin_status);
 		if (pid < 0) {
 			cmd_status = -EFAULT;
 			break;
+		}
+
+		if (builtin) {
+			if ((cmdlist->controlop == CMD_AND && !builtin_status) ||
+			    (cmdlist->controlop == CMD_OR && builtin_status)   ||
+			    (cmdlist->controlop == CMD_END)                    ||
+			    (cmdlist->controlop == CMD_SEMICOLON)              ||
+			    (cmdlist->controlop == CMD_PIPE)                   ||
+			    (cmdlist->controlop == CMD_BACKGROUND))
+				goto next;
+			else
+				goto out;
 		}
 
 		switch (cmdlist->controlop) {
 		case CMD_END:
 		case CMD_SEMICOLON:
 			if (waitpid(pid, &wp_status, 0) < 0)
-				die("waitpid fails");
+				die("Fail to wait");
 
 			if (WIFEXITED(wp_status))
 				cmd_status = WEXITSTATUS(wp_status);
