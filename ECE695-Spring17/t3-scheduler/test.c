d6985f332750d5c407f5dcf1d6b06be7b2f5c0e3
root <root@localhost.localdomain>
2017-03-29 13:26:51 -0400

    update test

--- ECE695-Spring17/t3-scheduler/test.c
+++ ECE695-Spring17/t3-scheduler/test.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sched.h>
 #include <sys/syscall.h>
 
 #define NR_SCHED_SETLIMIT	332
+#define SCHED_MYCFS		7
 
 int sched_setlimit(pid_t pid, int limit)
 {
 	syscall(NR_SCHED_SETLIMIT, pid, limit);
 }
 
+
 int main(void)
 {
-	sched_setlimit(0, 100);
+	struct sched_param param = { 0 };
+	pid_t pid;
+
+	printf("Old Policy: %d\n", sched_getscheduler(0));
+	sched_setscheduler(0, SCHED_MYCFS, &param);
+	printf("New Policy: %d\n", sched_getscheduler(0));
+
+	sched_setlimit(0, 50);
+
+	if ((pid = fork()) != 0) {
+		printf("pid: %d, policy: %d\n", getpid(), sched_getscheduler(0));
+	} else
+		printf("pid: %d, policy: %d\n", getpid(), sched_getscheduler(0));
 
 	return 0;
 }
